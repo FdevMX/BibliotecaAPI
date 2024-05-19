83797f30146d7b018c6f89deefa115ee2ede0357
Aldrin Aquino Sánchez <aldrin.aquino.san@outlook.com>
2024-05-19 14:44:46 -0600

    Se agrego models para Facturas

---
+++ Biblioteca.Models/Facturas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Facturas
+    {
+        [Key]
+        public int factura_id { get; set; }
+        public int prestamo_id { get; set; }
+        public DateOnly fecha_emision { get; set; }
+        public double monto_total { get; set; }
+        public string? detalles_pago { get; set; }
+        public string? estado_factura { get; set; }
+    }
+}
