ed51b586720c60060f6482c6bffce5a5164a8bfb
Alfredo Lopez Mendez <lopez-alfredo03@outlook.com>
2024-05-18 19:34:44 -0600

    interface y serivices para Libros

---
+++ Biblioteca.Core/BL/Interfaces/ILibros.cs
@@ -0,0 +1,21 @@
+﻿using Biblioteca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Core.BL.Interfaces
+{
+    // Nos permite hacer la abstraccion,
+    // o la herencia de funciones entre clases
+    public interface ILibros
+    {
+        // Task devuelve valores asyncronos y
+        // nos sirve para evitar el colapso del programa
+        Task<bool> GuardarLibro(Libros libros);
+        Task<bool> ActualizarLibro(Libros libros);
+        Task<bool> EliminarLibro(int libroId);
+        Task<List<Libros>> ListarLibros();
+    }
+}
