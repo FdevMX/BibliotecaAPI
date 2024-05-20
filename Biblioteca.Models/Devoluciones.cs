205def1a615c8224360273192b2fe87e310fa1a9
Daniel Castillo <DanielUvCas@outlook.com>
2024-05-19 18:22:24 -0600

    Se agrego modelo para devoluciones

---
+++ Biblioteca.Models/Devoluciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Models
+{
+    public class Devoluciones
+    {
+        [Key]
+        public int devolucion_id { get; set; }
+        public int prestamo_id { get; set; }
+        public DateOnly fecha_devolucion { get; set; }      
+        public string? estado_libro { get; set; }
+        public string? observaciones { get; set; }
+
+    }
+}
